de9f1eb973fc8a31c0568c2953afc49640712d76
Oleh Prymush <olegprimus5@gmail.com>
2023-05-03 18:10:51 +0300

    Added tasks

---
+++ C#/Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static int CountMeets(int[] hedgehogs, int color)
+        {
+            switch (color)
+            {
+                case 0:
+                    return hedgehogs[1] == hedgehogs[2] ? hedgehogs[1] : -1;
+                case 1:
+                    return hedgehogs[0] == hedgehogs[2] ? hedgehogs[0] : -1;
+                case 2:
+                    return hedgehogs[0] == hedgehogs[1] ? hedgehogs[0] : -1;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] hedgehogs = { 8, 10, 7 };
+            Console.WriteLine(CountMeets(hedgehogs, 2));
+        }
+    }
+}
---
+++ JS/script.js
@@ -0,0 +1,80 @@
+function validSolution(field){
+  for(let i = 0; i < 9; i++){
+    // check if we have 0 on field
+    if(field[i].includes(0)){
+     return false;
+    }
+
+    // check duplicates in lines of field
+    if(field[i].filter((item, index) => field[i].indexOf(item) !== index).length !== 0){
+      return false;
+    }
+
+    // check duplicates in columns of field
+    let columnArr = [];
+    for(let j = 0; j < 9; j++){
+      columnArr.push(field[j][i]);
+    }
+    if(columnArr.filter((item, index) => columnArr.indexOf(item) !== index).length !== 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+// check true value
+let res = validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]);
+console.log(res);
+
+//check false value dup in rows
+res = validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 2, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 9, 8, 6, 1, 7, 9]
+]);
+console.log(res);
+
+//check false value dup in columns
+res = validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 1, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]);
+console.log(res);
+  
+// check false zero
+res = validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]);
+
+console.log(res);
